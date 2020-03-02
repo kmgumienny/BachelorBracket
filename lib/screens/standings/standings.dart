5f858355eb61ebe888e949c8573236dc54da9069
Christopher <cwhimpenny@wpi.edu>
2020-03-02 15:55:11 -0500

    Added Tables to Standings Page

--- lib/screens/standings/standings.dart
+++ lib/screens/standings/standings.dart
@@ -16,6 +16,8 @@ class Standings extends StatefulWidget {
 
 class _StandingsState extends State<Standings> {
   var _points;
+  var userNames = [];
+  var userScores = [];
   int _week;
   Future<Object> getDetails() async {
     var firestore = Firestore.instance;
@@ -28,12 +30,26 @@ class _StandingsState extends State<Standings> {
     QuerySnapshot allUserTotals = 
         await firestore.collection("users").getDocuments();
 
-    DocumentSnapshot user2Details = allUserTotals.documents[0];
+    var i = 0;
+    while(allUserTotals.documents.length > i){
+      DocumentSnapshot user2Details = allUserTotals.documents[i];
+      user2Details.data.forEach((k, v) => {
+        userNames.add(k),
+        userScores.add(v),
+      });
 
+    
 
-    print(userDetails.data);
+      i++;
+    }
+    
+
+    print(userScores);
 
     return {
+      "numUsers": i,
+      "names": userNames,
+      "scores": userScores,
       "week": await adminDetails.data["week"],
       "total": await userDetails.data["total"],
       "points": await userDetails.data["points"]
@@ -54,7 +70,7 @@ class _StandingsState extends State<Standings> {
                   } else {
                     return Column(children: <Widget>[
                       Text("Your points: " + snapshot.data["total"].toStringAsFixed(2)),
-                      Text("Week number: " + snapshot.data["week"].toString()),
+                      Text("Week number: " + snapshot.data["userScores"].toString()),
                       Table(
                         border: TableBorder.all(),
                         children: [
@@ -140,6 +156,42 @@ class _StandingsState extends State<Standings> {
                           ]),
                         ]
                       ),
+                      Text('Standings'),
+                      Table(border: TableBorder.all(),
+                        children: [
+                          TableRow( children: [
+                            Column(children: [
+                              Text('UserName')
+                            ]),
+                            Column(children: [
+                              Text('Total Score:')
+                            ])
+                          ]),
+                          TableRow( children: [
+                            Column(children: [
+                              Text(userScores[1].toString())
+                            ]),
+                            Column(children: [
+                              Text(userScores[0].toString())
+                            ])
+                          ]),
+                          TableRow( children: [
+                            Column(children: [
+                              Text(userScores[5].toString())
+                            ]),
+                            Column(children: [
+                              Text(userScores[4].toString())
+                            ])
+                          ]),
+                          TableRow( children: [
+                            Column(children: [
+                              Text(userScores[9].toString())
+                            ]),
+                            Column(children: [
+                              Text(userScores[8].toString())
+                            ])
+                          ]),
+                        ]),
                     ]);
                   }
                 })));
